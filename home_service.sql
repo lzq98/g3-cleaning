a725c6e0a2db453b7bb324d8a7412f9485ace0bd
lzq98 <69748284+lzq98@users.noreply.github.com>
2022-11-15 20:31:20 +1030

    upload sql

---
+++ home_service.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主机： 172.17.0.1:3306
+-- 生成日期： 2022-11-15 09:59:53
+-- 服务器版本： 8.0.30
+-- PHP 版本： 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 数据库： `home_service`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `customer`
+--
+
+CREATE TABLE `customer` (
+  `uid` int UNSIGNED NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- 转存表中的数据 `customer`
+--
+
+INSERT INTO `customer` (`uid`, `email`, `username`, `name`, `phone`, `password`) VALUES
+(1, 'user@test.com', 'testuser', 'Test User', '0412345678', '098f6bcd4621d373cade4e832627b4f6');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `orders`
+--
+
+CREATE TABLE `orders` (
+  `oid` int UNSIGNED NOT NULL,
+  `customer` int UNSIGNED NOT NULL,
+  `worker` int UNSIGNED NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `job` varchar(300) NOT NULL,
+  `comment` varchar(300) NOT NULL,
+  `price` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `worker`
+--
+
+CREATE TABLE `worker` (
+  `uid` int UNSIGNED NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `healthy` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- 表的索引 `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`oid`);
+
+--
+-- 表的索引 `worker`
+--
+ALTER TABLE `worker`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `uid` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用表AUTO_INCREMENT `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `oid` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用表AUTO_INCREMENT `worker`
+--
+ALTER TABLE `worker`
+  MODIFY `uid` int UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
