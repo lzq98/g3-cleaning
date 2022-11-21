338fc3c4e91e250773bdbfe9e0ea442b690ae498
Zhengjie Jiang <jzjjiangzj@gmail.com>
2022-11-21 17:23:22 +0800

    homepage text modified
    modified text for g3_cleaning

--- index.php
+++ index.php
@@ -105,26 +105,23 @@
                 <div class="row content">
                     <div class="col-lg-6">
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore
-                            magna aliqua.
+                            G3 Cleaning was founded in Adelaide in 2022 and is exbanding with strong growth.
+                            We aim:
                         </p>
                         <ul>
-                            <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat</li>
-                            <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate
-                                velit</li>
-                            <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat</li>
+                            <li><i class="ri-check-double-line"></i> To offer Adelaide house cleaning customers a premium service.</li>
+                            <li><i class="ri-check-double-line"></i> To provide a geniuine business opportunity for committed franchisees.</li>
+                            <li><i class="ri-check-double-line"></i> To protect public health under Covid-19 situation.</li>
                         </ul>
                     </div>
                     <div class="col-lg-6 pt-4 pt-lg-0">
                         <p>
-                            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate
-                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                            proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
+                        A professionally-trained self-employed house cleaner will strive for excellence every time.
+                        </p>
+                        <p>
+                        Our vision is to continually offer the best quality home cleaning available in Adelaide SA.  
+                        By supporting our franchisees to become the best home cleaner they can, we are able to pursue 
+                        a higher standard of customer service and provide value to our customers every time.
                         </p>
                         <a href="#" class="btn-learn-more">Learn More</a>
                     </div>
@@ -143,10 +140,11 @@
                         class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
 
                         <div class="content">
-                            <h3>Eum ipsam laborum deleniti <strong>velit pariatur architecto aut nihil</strong></h3>
+                            <h3>We Provide our customers with <strong>the safest cleaning service</strong></h3>
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                                COVID-19 is now a global pandemic affecting over 200 countries, 
+                                after its first recorded outbreak in December 2019.
+                                Choosing G3-Cleaning is the safest and easiest way to keep your home clean.
                             </p>
                         </div>
 
@@ -154,45 +152,41 @@
                             <ul>
                                 <li>
                                     <a data-bs-toggle="collapse" class="collapse"
-                                        data-bs-target="#accordion-list-1"><span>01</span> Non consectetur a erat nam at
-                                        lectus urna duis? <i class="bx bx-chevron-down icon-show"></i><i
+                                        data-bs-target="#accordion-list-1">Can I book a service
+                                        for a certain day? <i class="bx bx-chevron-down icon-show"></i><i
                                             class="bx bx-chevron-up icon-close"></i></a>
                                     <div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
                                         <p>
-                                            Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus
-                                            laoreet non curabitur gravida. Venenatis lectus magna fringilla urna
-                                            porttitor rhoncus dolor purus non.
+                                            Ofcourse you can!  G3-Cleaning aims to provid our customers with
+                                            premium quality services and best price.  With an eligible account and you are good to go!
                                         </p>
                                     </div>
                                 </li>
 
                                 <li>
                                     <a data-bs-toggle="collapse" data-bs-target="#accordion-list-2"
-                                        class="collapsed"><span>02</span> Feugiat scelerisque varius morbi enim nunc? <i
+                                        class="collapsed">Worried of getting infected by Covid? <i
                                             class="bx bx-chevron-down icon-show"></i><i
                                             class="bx bx-chevron-up icon-close"></i></a>
                                     <div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
                                         <p>
-                                            Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id
-                                            interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus
-                                            scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper
-                                            dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                                            All our providers are required to be licensed and, more importantly, Covid negative! 
+                                            You can always ask for proof of vaccination or Covid test result from your service provider!
+                                            
                                         </p>
                                     </div>
                                 </li>
 
                                 <li>
                                     <a data-bs-toggle="collapse" data-bs-target="#accordion-list-3"
-                                        class="collapsed"><span>03</span> Dolor sit amet consectetur adipiscing elit? <i
+                                        class="collapsed">What service can I book for? <i
                                             class="bx bx-chevron-down icon-show"></i><i
                                             class="bx bx-chevron-up icon-close"></i></a>
                                     <div id="accordion-list-3" class="collapse" data-bs-parent=".accordion-list">
                                         <p>
-                                            Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci.
-                                            Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet
-                                            nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis
-                                            convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio
-                                            morbi quis
+                                            G3-Cleaning provides our customers with cleaning services 
+                                            including once-off cleaning, regular cleaning, spring cleaning and more!
+                                            Register and book your next service with us now!
                                         </p>
                                     </div>
                                 </li>
@@ -219,17 +213,16 @@
                         <img src="assets/img/skills.png" class="img-fluid" alt="">
                     </div>
                     <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
-                        <h3>Voluptatem dignissimos provident quasi corporis voluptates</h3>
+                        <h3>Our customers are satisfied!</h3>
                         <p class="fst-italic">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore
-                            magna aliqua.
+                            We have a large customer base in Adelaide and 
+                            feedbacks are overwhelmingly good!
                         </p>
 
                         <div class="skills-content">
 
                             <div class="progress">
-                                <span class="skill">HTML <i class="val">100%</i></span>
+                                <span class="skill">Good <i class="val">38%</i></span>
                                 <div class="progress-bar-wrap">
                                     <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0"
                                         aria-valuemax="100"></div>
@@ -237,7 +230,7 @@
                             </div>
 
                             <div class="progress">
-                                <span class="skill">CSS <i class="val">90%</i></span>
+                                <span class="skill">Excellent <i class="val">23%</i></span>
                                 <div class="progress-bar-wrap">
                                     <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0"
                                         aria-valuemax="100"></div>
@@ -245,7 +238,7 @@
                             </div>
 
                             <div class="progress">
-                                <span class="skill">JavaScript <i class="val">75%</i></span>
+                                <span class="skill">Okay <i class="val">12%</i></span>
                                 <div class="progress-bar-wrap">
                                     <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
                                         aria-valuemax="100"></div>
@@ -253,7 +246,7 @@
                             </div>
 
                             <div class="progress">
-                                <span class="skill">Photoshop <i class="val">55%</i></span>
+                                <span class="skill">Nice <i class="val">10%</i></span>
                                 <div class="progress-bar-wrap">
                                     <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0"
                                         aria-valuemax="100"></div>
@@ -282,7 +275,7 @@
                 <div class="row">
                     <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
                         <div class="icon-box">
-                            <div class="icon"><i class="bx bxl-dribbble"></i></div>
+                            <div class="icon"><img src= "assets/img/Clean/Regular-Cleaning.png" height="200" width="200"></div>
                             <h4><a href="">Lorem Ipsum</a></h4>
                             <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
                         </div>
@@ -291,7 +284,7 @@
                     <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in"
                         data-aos-delay="200">
                         <div class="icon-box">
-                            <div class="icon"><i class="bx bx-file"></i></div>
+                            <div class="icon"><img src= "assets/img/Clean/Window-Cleaning.png" height="200" width="200"></div>
                             <h4><a href="">Sed ut perspici</a></h4>
                             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
                         </div>
@@ -300,7 +293,7 @@
                     <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
                         data-aos-delay="300">
                         <div class="icon-box">
-                            <div class="icon"><i class="bx bx-tachometer"></i></div>
+                            <div class="icon"><img src= "assets/img/Clean/Carpet-Cleaning.png" height="200" width="200"></div>
                             <h4><a href="">Magni Dolores</a></h4>
                             <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
                         </div>
@@ -309,7 +302,7 @@
                     <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
                         data-aos-delay="400">
                         <div class="icon-box">
-                            <div class="icon"><i class="bx bx-layer"></i></div>
+                            <div class="icon"><img src= "assets/img/Clean/End-of-Lease.png" height="200" width="200"></div>
                             <h4><a href="">Nemo Enim</a></h4>
                             <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
                         </div>
